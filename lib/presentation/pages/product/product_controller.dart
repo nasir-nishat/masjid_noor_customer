f32eb64a5d88c0d70dff5b4e65286ad743dc42fc
Nishat Nasir <nishatnasir00@gmail.com>
2024-07-21 12:16:18 +0900

    Updated Bank details

--- lib/presentation/pages/product/product_controller.dart
+++ lib/presentation/pages/product/product_controller.dart
@@ -29,6 +29,7 @@ class ProductController extends GetxController {
   @override
   void onInit() {
     super.onInit();
+    PrayerTimesController.to.getBankDetails();
     fetchNewProducts();
     fetchPopularProducts();
     getCategories();
