df34bb0cd74b5a8cdf645c44a721902fd08c5fcf
Nishat Nasir <nishatnasir00@gmail.com>
2024-08-24 16:30:54 +0900

    Updated categories Hide on scroll down

--- lib/presentation/pages/product/product_list_page.dart
+++ lib/presentation/pages/product/product_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:masjid_noor_customer/presentation/pages/product/product_controller.dart';
@@ -6,9 +7,10 @@ import '../../../navigation/router.dart';
 import '../../widgets/product_item.dart';
 
 class ProductListPage extends GetView<ProductController> {
-  ProductListPage({super.key});
+  ProductListPage({Key? key}) : super(key: key);
 
   final ScrollController _scrollController = ScrollController();
+  final RxBool _showCategories = true.obs;
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +21,14 @@ class ProductListPage extends GetView<ProductController> {
           _scrollController.position.pixels ==
               _scrollController.position.maxScrollExtent) {
         controller.loadMoreProducts();
+      }
+
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        _showCategories.value = false;
+      } else if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.forward) {
+        _showCategories.value = true;
       }
     });
 
@@ -31,21 +41,15 @@ class ProductListPage extends GetView<ProductController> {
           },
           child: Column(
             children: [
-              SizedBox(
-                height: 50.h,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: controller.categories.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.w),
-                      child: getCats(index),
-                    );
-                  },
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                height: _showCategories.value ? 50.h : 0,
+                child: SingleChildScrollView(
+                  child: _buildCategories(),
                 ),
               ),
               SizedBox(height: 10.h),
-              Expanded(child: getProds()),
+              Expanded(child: _buildProductGrid()),
             ],
           ),
         ),
@@ -53,64 +57,78 @@ class ProductListPage extends GetView<ProductController> {
     );
   }
 
-  Widget getCats(int index) {
-    bool isSelected =
-        controller.selectedCategory.value == controller.categories[index];
-    return ChoiceChip(
-      padding: EdgeInsets.symmetric(horizontal: 8.w),
-      backgroundColor: isSelected ? Colors.blue : Colors.transparent,
-      selectedColor: Colors.blue,
-      side: isSelected ? BorderSide.none : const BorderSide(color: Colors.grey),
-      showCheckmark: false,
-      label: Text(
-        controller.categories[index].name,
-        style: TextStyle(
-          color: isSelected ? Colors.white : Colors.black,
-        ),
+  Widget _buildCategories() {
+    return SizedBox(
+      height: 50.h,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: controller.categories.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.w),
+            child: _buildCategoryChip(index),
+          );
+        },
       ),
-      selected: isSelected,
-      onSelected: (isSelected) {
-        if (isSelected) {
-          controller.selectedCategory.value = controller.categories[index];
-          controller.fetchProductsByCategory();
-        }
-      },
     );
   }
 
-  Widget getProds() {
+  Widget _buildCategoryChip(int index) {
+    bool isSelected =
+        controller.selectedCategory.value == controller.categories[index];
+    return Obx(() => ChoiceChip(
+          padding: EdgeInsets.symmetric(horizontal: 8.w),
+          backgroundColor: isSelected ? Colors.blue : Colors.transparent,
+          selectedColor: Colors.blue,
+          side: isSelected
+              ? BorderSide.none
+              : const BorderSide(color: Colors.grey),
+          showCheckmark: false,
+          label: Text(
+            controller.categories[index].name,
+            style: TextStyle(
+              color: isSelected ? Colors.white : Colors.black,
+            ),
+          ),
+          selected: isSelected,
+          onSelected: (isSelected) {
+            if (isSelected) {
+              controller.selectedCategory.value = controller.categories[index];
+              controller.fetchProductsByCategory();
+            }
+          },
+        ));
+  }
+
+  Widget _buildProductGrid() {
     if (controller.isLoading.value && controller.products.isEmpty) {
       return const Center(child: CircularProgressIndicator());
+    } else if (controller.products.isEmpty) {
+      return const Center(child: Text('No products found'));
     } else {
-      return controller.products.isEmpty
-          ? const Center(child: Text('No products found'))
-          : GridView.builder(
-              padding: EdgeInsets.symmetric(horizontal: 12.w),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10.w,
-                mainAxisSpacing: 10.h,
-                childAspectRatio: 0.8,
-              ),
-              itemCount: controller.products.length +
-                  (controller.isLoading.value ? 2 : 0),
-              itemBuilder: (context, index) {
-                if (index >= controller.products.length) {
-                  if (controller.isLoading.value) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else {
-                    return const SizedBox.shrink();
-                  }
-                }
-                return ProductItem(
-                  product: controller.products[index],
-                  parentRoute: Routes.products,
-                );
-              },
-              controller: _scrollController,
-            );
+      return GridView.builder(
+        padding: EdgeInsets.symmetric(horizontal: 12.w),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.w,
+          mainAxisSpacing: 10.h,
+          childAspectRatio: 0.8,
+        ),
+        itemCount:
+            controller.products.length + (controller.isLoading.value ? 2 : 0),
+        itemBuilder: (context, index) {
+          if (index >= controller.products.length) {
+            return controller.isLoading.value
+                ? const Center(child: CircularProgressIndicator())
+                : const SizedBox.shrink();
+          }
+          return ProductItem(
+            product: controller.products[index],
+            parentRoute: Routes.products,
+          );
+        },
+        controller: _scrollController,
+      );
     }
   }
 }
