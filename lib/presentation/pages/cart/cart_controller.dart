0def42215e5011787fe56f960799d72cf97df4bf
nishat-nasir <nishatnasir00@gmail.com>
2024-09-10 11:01:12 +0900

    Updated Cart and added to local

--- lib/presentation/pages/cart/cart_controller.dart
+++ lib/presentation/pages/cart/cart_controller.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:masjid_noor_customer/mgr/models/order_md.dart';
+import 'package:masjid_noor_customer/mgr/models/cart_md.dart'; // Ensure correct import path
 import 'package:masjid_noor_customer/presentation/pages/all_export.dart';
 
 class CartController extends GetxController {
@@ -13,6 +17,14 @@ class CartController extends GetxController {
   PaymentMethod? paymentMethod;
   var contactNumber = '';
   var note = '';
+
+  final String _cartKey = 'cart_items';
+
+  @override
+  void onInit() {
+    super.onInit();
+    _loadCartData();
+  }
 
   int get totalProdCount {
     return cartItems.fold(0, (total, cartItem) => total + cartItem.quantity);
@@ -30,31 +42,52 @@ class CartController extends GetxController {
     } else {
       cartItems.add(CartMd(product: product, quantity: 1));
     }
+    _saveCartData();
   }
 
   void clearCart() {
     cartItems.clear();
-    cartItems.refresh();
+    _saveCartData();
   }
 
   void removeFromCart(CartMd cartItem) {
     cartItems.remove(cartItem);
+    _saveCartData();
   }
 
   void increaseQuantity(CartMd cartItem) {
     cartItem.quantity++;
     cartItems.refresh();
+    _saveCartData();
   }
 
   void decreaseQuantity(CartMd cartItem) {
     if (cartItem.quantity > 1) {
       cartItem.quantity--;
       cartItems.refresh();
+      _saveCartData();
     }
   }
 
   double calculateTotalPrice() {
     return cartItems.fold(0, (total, cartItem) => total + cartItem.totalPrice);
+  }
+
+  Future<void> _saveCartData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> cartItemsJson =
+        cartItems.map((item) => jsonEncode(item.toJson())).toList();
+    await prefs.setStringList(_cartKey, cartItemsJson);
+  }
+
+  Future<void> _loadCartData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? cartItemsJson = prefs.getStringList(_cartKey);
+    if (cartItemsJson != null) {
+      cartItems.value = cartItemsJson
+          .map((item) => CartMd.fromJson(jsonDecode(item)))
+          .toList();
+    }
   }
 
   Future<bool> processOrder(BuildContext context) async {
@@ -66,7 +99,6 @@ class CartController extends GetxController {
     String userId = SupabaseDep.impl.supabase.auth.currentUser?.id ?? '';
     if (userId.isEmpty) {
       AppController.to.hideGlobalLoading();
-      // showSnackBar(context, 'Please login to place order');
       showToast('Please login to place order', isWarning: true);
       return false;
     }
